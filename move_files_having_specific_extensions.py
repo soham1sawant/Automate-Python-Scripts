97aac466ba3bb337464813020f278dcec9c0d29d
Soham Sawant <geekysoham@gmail.com>
2019-12-19 12:22:58 +0530

    Added proper indentation to output

--- move_files_having_specific_extensions.py
+++ move_files_having_specific_extensions.py
@@ -12,24 +12,29 @@ import os , shutil
 
 def compute(path, fileExt):
     if os.path.exists(path):          # checks if the path entered by the user exists
+        print()
         print("The path exists")
 
         for foldername, subfolder, filename in os.walk(path):       # walks the each subfolder in the specified folder
             for file in filename:
                 if file.endswith(fileExt):
                     print(os.path.join(foldername , file))
+        print()
         sure_prompt = input("These files will be moved, are you sure? (Y/N) : ").strip().upper() # asks the user if he is sure about deleting the files
 
         if sure_prompt == 'Y':
+            print()
             move_path = input("Enter the path to  move the files into : ")
+
             if os.path.exists(move_path):
+                print()
                 print("Moving......")
 
                 for foldername, subfolder, filename in os.walk(path):       # walks the each subfolder in the specified folder
                     for file in filename:
                         if file.endswith(fileExt):
-                            #shutil.move(os.path.join(foldername , file) , os.path.join(move_path , file)) # moves the file to the specified location without chaanging its filename
-                            print(os.path.join(foldername , file) + ' moved to : ' + os.path.join(move_path , file))
+                            shutil.move(os.path.join(foldername , file) , os.path.join(move_path , file)) # moves the file to the specified location without chaanging its filename
+                            #print(os.path.join(foldername , file) + ' moved to : ' + os.path.join(move_path , file))
             else:
                 print("The destination path does not exist !!!")
 
@@ -48,6 +53,6 @@ if __name__ == '__main__':
     print()
 
     path = input("Enter the full path of the folder to search : ")    # Take the path of folder to search from the user
-    fileExt = input("Enter the extension of the file to serach : ") # Take the file extension to search for from the user
+    fileExt = input("Enter the extension of the file to search : ") # Take the file extension to search for from the user
 
     compute(path , fileExt)
