20bddc6bbf9a4cb337b8a5fe2d1f3fdeafaf445f
Soham Sawant <geekysoham@gmail.com>
2019-12-24 19:11:39 +0530

    Initial commit

---
+++ download_google_images.py
@@ -0,0 +1,13 @@
+# download_google_images.py
+
+# Program to download the 10 images on the topic specified by the user from google
+
+# TODO : Take the topic from the topic
+
+# TODO : Search the web for the images
+
+# TODO : Take the path of the folder to save the images to from the user
+
+# TODO : Download the images
+
+# TODO : Inform the user that the images have finished downloading
