652c93296f35e4734686b823e07250a7cd6f15ef
Soham Sawant <geekysoham@gmail.com>
2019-12-25 11:13:50 +0530

    Searches google.com for the topic

--- download_google_images.py
+++ download_google_images.py
@@ -2,12 +2,30 @@
 
 # Program to download the 10 images on the topic specified by the user from google
 
-# TODO : Take the topic from the topic
-
-# TODO : Search the web for the images
-
 # TODO : Take the path of the folder to save the images to from the user
 
 # TODO : Download the images
 
 # TODO : Inform the user that the images have finished downloading
+
+import bs4 , requests , os
+from selenium import webdriver
+
+def download(topic , save):
+    browser = webdriver.Firefox()
+    browser.get('http://google.com')
+    textBox = browser.find_element_by_css_selector('html body#gsr.hp.vasq.big div#viewport.ctr-p div#searchform.jhp.big form#tsf.tsf.nj div div.A8SBwf div.RNNXgb div.SDkEP div.a4bIc input.gLFyf.gsfi')
+    textBox.send_keys(topic)
+    textBox.submit()                        #Search the web for the images
+    
+
+print("Program to download images from google")
+print()
+
+topic = input("Enter the topic for downloading the images : ")      # Takes the topic from the topic
+savePath = input("Enter the path of the folder to save the downloaded images : ")
+
+if os.path.exists(savePath):
+    def download(topic , savePath)
+else:
+    print("The path doe not exists !")
